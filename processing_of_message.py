897b2299690af517398194b4b61268817bafb21d
Mik-404 <89186196+Mik-404@users.noreply.github.com>
2022-12-17 15:28:25 +0300

    Delete processing_of_message.py

--- processing_of_message.py
+++
@@ -1,7 +0,0 @@
-class ProcessingOfMessage:
-    def __init__(self, message_text, type_update):
-        self.message_text = message_text
-        self.type_update = type_update
-
-    def answer_text(self):
-        return "UwU"
