c3c8372deb48743929a599e9d14bfb929746febc
Mik-404 <b.bcftd@yandex.ru>
2022-12-22 20:46:18 +0300

    v1.11

--- settings.py
+++ settings.py
@@ -15,10 +15,11 @@ BASE_SETTING_CHAT_IN_DATABASE = {
     "style": "random",
     "p": '50',
 }
-STYLES = ['rude', 'yoda', 'scientific', 'cute', 'random']
+STYLES = ['rude', 'yoda', 'scientific', 'cute', 'old_rus', 'random']
 MODELS = {
     'rude': '633b6955f7afbca241e611ae313d83ee',
     'cute': 'dfa074d119af6e9e15a5f18717b98941',
     'scientific': '9e68215a87e86320a044f41907e1e8d5',
-    'yoda': '2ed91ea1878c4f08ffd57a4ae32bb1b5'
+    'yoda': '2ed91ea1878c4f08ffd57a4ae32bb1b5',
+    'old_rus': '77a0b80f1962ccac71d3544c16efca89'
 }
