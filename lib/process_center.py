84916f7562f2c95e6d4defbcda9d516a101d4e44
Mik-404 <b.bcftd@yandex.ru>
2022-12-23 19:37:27 +0300

    final round 2 (cool phonk)

--- lib/process_center.py
+++ lib/process_center.py
@@ -1,6 +1,3 @@
-import asyncio
-import copy
-
 import settings
 from lib import texts, message_processing
 from lib.utils import *
@@ -10,16 +7,19 @@ generator = message_processing.ProcessingModel()
 
 
 async def distribution_center(message_type, message_text):
-    if not sem.locked():
-        async with sem:
-            loop = asyncio.get_event_loop()
-            for i in range(2):
-                future = loop.create_future()
-                loop.create_task(generator.generate(message_type, message_text, future))
-                await future
-                gen = future.result()
-                if gen != '-':
-                    return gen
-            return texts.so_har_msg
-    else:
+    try:
+        if not sem.locked():
+            async with sem:
+                loop = asyncio.get_event_loop()
+                for i in range(2):
+                    future = loop.create_future()
+                    loop.create_task(generator.generate(message_type, message_text, future))
+                    await future
+                    gen = future.result()
+                    if gen != '-':
+                        return gen
+                return texts.so_har_msg
+        else:
+            return texts.too_many_reauests
+    except:
         return texts.too_many_reauests
